093193d357f25245f8c2b12b7181c83ead196ca3
Salim66 <salimhasanriad8@gmail.com>
2022-01-21 22:10:50 +0600

    Adding toastr In for view message

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -44,7 +44,13 @@ class AdminProfileController extends Controller
             $data['profile_photo_path'] = $filename;
         }
         $data->save();
-        return redirect()->route('admin.profile');
+
+        $notification = [
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->route('admin.profile')->with($notification);
     }
 
 }
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -12,6 +12,8 @@
 
 	<!-- Vendors Style-->
 	<link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
+    <!-- Toastr CSS -->
+	<link rel="stylesheet" href="{{ asset('backend/css/toastr.min.css') }}">
 
 	<!-- Style-->
 	<link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
@@ -62,6 +64,31 @@
 	<!-- Sunny Admin App -->
 	<script src="{{ asset('backend/js/template.js') }}"></script>
 	<script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
+
+    <!-- Toastr JS -->
+	<script src="{{ asset('backend/js/toastr.min.js') }}"></script>
+    <script type="text/javascript">
+        @if(Session::has('message'))
+         let type = "{{ Session::get('alert-type', 'info') }}"
+         switch(type){
+            case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+
+            case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+
+            case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+
+              case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break;
+        }
+        @endif
+    </script>
 
 
 </body>
