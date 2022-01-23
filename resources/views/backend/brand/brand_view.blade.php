ca7a5e4539c5aed50b1f58d1647224a465c1754f
Salim66 <salimhasanriad8@gmail.com>
2022-01-23 15:11:08 +0600

    Brand page desing part 2

--- resources/views/backend/brand/brand_view.blade.php
+++ resources/views/backend/brand/brand_view.blade.php
@@ -1,6 +1,58 @@
 @extends('admin.admin_master')
 
 @section('main-content')
+<div class="container-full">
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
 
 
+        <div class="col-12">
+
+         <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Brand List</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Brand En</th>
+                            <th>Brand Ar</th>
+                            <th>Image</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($brands as $data)
+                        <tr>
+                            <td>{{ $data->brand_name_en }}</td>
+                            <td>{{ $data->brand_name_ar }}</td>
+                            <td>
+                                <img src="{{ URL::to('upload/brands/') }}/{{ $data->brand_image }}" alt="">
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-info">Edit</a>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+        </div>
+
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+</div>
 @endsection
