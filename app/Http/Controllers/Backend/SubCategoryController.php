97cb73d97d4571f6f4d4b648593e25312137ec17
Salim66 <salimhasanriad8@gmail.com>
2022-01-23 23:17:12 +0600

    SubCategory Crud Part 1

---
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+
+class SubCategoryController extends Controller
+{
+    /**
+     * Category view page
+     */
+    public function subCategoryView(){
+        $subcategories = SubCategory::latest()->get();
+        return view('backend.category.sub_category_view', compact('subcategories'));
+    }
+
+    /**
+     * Category store
+     */
+    public function categoryStore(Request $request){
+
+        // Validation
+        $request->validate([
+            'category_name_en' => 'required',
+            'category_name_ar' => 'required',
+            'category_icon' => 'required'
+        ],
+        [
+            'category_name_en.required' => 'The category name english is required!',
+            'category_name_ar.required' => 'The category name arabic is required!'
+        ]);
+
+        Category::create([
+            'category_name_en' => $request->category_name_en,
+            'category_name_ar' => $request->category_name_ar,
+            'category_slug_en' => strtolower(str_replace(' ', '-', $request->category_name_en)),
+            'category_slug_ar' => strtolower(str_replace(' ', '-', $request->category_name_ar)),
+            'category_icon'    => $request->category_icon
+        ]);
+
+        $notification = [
+            'message' => 'Category Added Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->back()->with($notification);
+
+    }
+
+    /**
+     * Category edit
+     */
+    public function categoryEdit($id){
+        $data = Category::findOrFail($id);
+        return view('backend.category.category_edit', compact('data'));
+    }
+
+    /**
+     * Category update
+     */
+    public function categoryUpdate(Request $request){
+
+        $category_id = $request->id;
+
+        Category::findOrFail($category_id)->update([
+            'category_name_en' => $request->category_name_en,
+            'category_name_ar' => $request->category_name_ar,
+            'category_slug_en' => strtolower(str_replace(' ', '-', $request->category_name_en)),
+            'category_slug_ar' => strtolower(str_replace(' ', '-', $request->category_name_ar)),
+            'category_icon'    => $request->category_icon
+        ]);
+
+        $notification = [
+            'message' => 'Category Updated Successfully',
+            'alert-type' => 'info'
+        ];
+
+        return redirect()->route('all.category')->with($notification);
+
+    }
+
+    /**
+     * Category Delete
+     */
+    public function categoryDelete($id){
+
+        Category::findOrFail($id)->delete();
+
+        $notification = [
+            'message' => 'Category Deleted Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->back()->with($notification);
+
+    }
+}
