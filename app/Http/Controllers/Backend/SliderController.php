891a6c5fd891f7d3b6424c08204465a0fe5c095f
Salim66 <salimhasanriad8@gmail.com>
2022-01-26 21:10:49 +0600

    Upload Slider and Show All Slider List Part 2

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Image;
 
 class SliderController extends Controller
 {
@@ -14,6 +15,41 @@ class SliderController extends Controller
     public function sliderView(){
         $sliders = Slider::latest()->get();
         return view('backend.slider.slider_view', compact('sliders'));
+    }
+
+    /**
+     * Slider store
+     */
+    public function sliderStore(Request $request){
+
+        // Validation
+        $request->validate([
+            'slider_img' => 'required'
+        ],
+        [
+            'slider_img.required' => 'Please insert one image'
+        ]);
+
+        $image = $request->file('slider_img');
+        $name_gen = hexdec(uniqid()).'.'. $image->getClientOriginalExtension();
+        Image::make($image)->resize(550, 600)->save('upload/slider/'. $name_gen);
+        $save_url = 'upload/slider/'.$name_gen;
+
+        Slider::create([
+            'slider_title_en' => $request->slider_title_en,
+            'slider_title_ar' => $request->slider_title_ar,
+            'slider_descp_en' => $request->slider_descp_en,
+            'slider_descp_ar' => $request->slider_descp_ar,
+            'slider_img'   => $save_url
+        ]);
+
+        $notification = [
+            'message' => 'Slider Added Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->back()->with($notification);
+
     }
 
 }
--- resources/views/backend/slider/slider_view.blade.php
+++ resources/views/backend/slider/slider_view.blade.php
@@ -11,7 +11,7 @@
 
          <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">Brand List</h3>
+              <h3 class="box-title">Slider List</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
@@ -19,26 +19,50 @@
                   <table id="example1" class="table table-bordered table-striped">
                     <thead>
                         <tr>
-                            <th>Brand En</th>
-                            <th>Brand Ar</th>
-                            <th>Image</th>
+                            <th>Slider Img</th>
+                            <th>Title En</th>
+                            <th>Descp En</th>
+                            <th>Status</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {{-- @foreach($brands as $data)
+                        @foreach($sliders as $data)
                         <tr>
-                            <td>{{ $data->brand_name_en }}</td>
-                            <td>{{ $data->brand_name_ar }}</td>
                             <td>
-                                <img class="brand__img-table" src="{{ URL::to('/') }}/{{ $data->brand_image }}" alt="">
+                                <img class="brand__img-table" src="{{ URL::to($data->slider_img) }}" alt="">
                             </td>
                             <td>
-                                <a title="Edit Data" href="{{ route('brand.edit', $data->id) }}" class="btn btn-info"><i class="fa fa-pencil"></i></a>
-                                <a title="Delete Data" href="{{ route('brand.delete', $data->id) }}" id="delete" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                @if($data->slider_title_en == NULL)
+                                <span class="badge badge-pill badge-danger">No Title</span>
+                                @else
+                                {{ $data->slider_title_en }}</td>
+                                @endif
+                            </td>
+                            <td>
+                                @if($data->slider_descp_en == NULL)
+                                <span class="badge badge-pill badge-danger">No Description</span>
+                                @else
+                                {{ $data->slider_descp_en }}</td>
+                                @endif
+                            <td>
+                                @if($data->status == 1)
+                                <span class="badge badge-pill badge-success">Active</span>
+                                @else
+                                <span class="badge badge-pill badge-danger">Inactive</span>
+                                @endif
+                            </td>
+                            <td width="30%">
+                                <a title="Edit Data" href="{{ route('slider.edit', $data->id) }}" class="btn btn-info btn-sm"><i class="fa fa-pencil"></i></a>
+                                <a title="Delete Data" href="{{ route('slider.delete', $data->id) }}" id="delete" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                @if($data->status == 1)
+                                <a title="Inactive Slider" href="{{ route('product.inactive', $data->id) }}" class="btn btn-danger btn-sm"><i class="fa fa-arrow-down"></i></a>
+                                @else
+                                <a title="Active Slider" href="{{ route('product.active', $data->id) }}" class="btn btn-success btn-sm"><i class="fa fa-arrow-up"></i></a>
+                                @endif
                             </td>
                         </tr>
-                        @endforeach --}}
+                        @endforeach
 
                     </tbody>
                   </table>
@@ -50,41 +74,47 @@
 
         <!-- /.col -->
 
-        <!-- Add Brand Page  -->
+        <!-- Add Slider Page  -->
         <div class="col-4">
 
             <div class="box">
                <div class="box-header with-border">
-                 <h3 class="box-title">Add Brand</h3>
+                 <h3 class="box-title">Add Slider</h3>
                </div>
                <!-- /.box-header -->
                <div class="box-body">
                    <div class="table-responsive">
-                        <form action="{{ route('brand.store') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('slider.store') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="form-group">
-                                <h5>Brand Name English <span class="text-danger">*</span></h5>
+                                <h5>Slider Title English <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="brand_name_en" class="form-control">
-                                    @error('brand_name_en')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                                    <input type="text" name="slider_title_en" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <h5>Brand Name Arabic <span class="text-danger">*</span></h5>
+                                <h5>Slider Title Arabic <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="brand_name_ar" class="form-control">
-                                    @error('brand_name_ar')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                                    <input type="text" name="slider_title_ar" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <h5>Brand Image <span class="text-danger">*</span></h5>
+                                <h5>Slider Description English<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="file" name="brand_image" class="form-control">
-                                    @error('brand_image')
+                                    <textarea name="slider_descp_en" id="textarea" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <h5>Slider Description Arabic<span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <textarea name="slider_descp_ar" id="textarea" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <h5>Slider Image <span class="text-danger">*</span></h5>
+                                <div class="controls">
+                                    <input type="file" name="slider_img" class="form-control">
+                                    @error('slider_img')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
