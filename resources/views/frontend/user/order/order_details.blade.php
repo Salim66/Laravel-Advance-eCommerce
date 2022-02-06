084060b3873f088758c02943e05233220cfc1224
Salim66 <salimhasanriad8@gmail.com>
2022-02-06 17:56:09 +0600

    View Users Order In Profile Part 2

--- resources/views/frontend/user/order/order_details.blade.php
+++ resources/views/frontend/user/order/order_details.blade.php
@@ -5,10 +5,59 @@
     <div class="container user-container">
         <div class="row">
             @include('frontend.common.user_sidebar')
-            <div class="col-md-1"></div>
-            <div class="col-md-9 my-4">
+            <div class="col-md-5 my-4">
 
-                
+                <div class="card">
+                    <div class="card-header"><h4>Shipping Details</h4></div>
+                   <hr>
+                   <div class="card-body shipping_details">
+                     <table class="table">
+                      <tr>
+                        <th> Shipping Name : </th>
+                         <th> {{ $order->name }} </th>
+                      </tr>
+          
+                       <tr>
+                        <th> Shipping Phone : </th>
+                         <th> {{ $order->phone }} </th>
+                      </tr>
+          
+                       <tr>
+                        <th> Shipping Email : </th>
+                         <th> {{ $order->email }} </th>
+                      </tr>
+          
+                       <tr>
+                        <th> Division : </th>
+                         <th> {{ $order->division->division_name }} </th>
+                      </tr>
+          
+                       <tr>
+                        <th> District : </th>
+                         <th> {{ $order->district->district_name }} </th>
+                      </tr>
+          
+                       <tr>
+                        <th> State : </th>
+                         <th>{{ $order->state->state_name }} </th>
+                      </tr>
+          
+                      <tr>
+                        <th> Post Code : </th>
+                         <th> {{ $order->post_code }} </th>
+                      </tr>
+          
+                      <tr>
+                        <th> Order Date : </th>
+                         <th> {{ $order->order_date }} </th>
+                      </tr>
+                       
+                     </table>
+          
+          
+                   </div> 
+                    
+                </div>
 
             </div>
         </div>
