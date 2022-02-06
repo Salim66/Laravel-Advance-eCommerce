9539cbaf3693d1d5e68f9f0c24f51600aa841267
Salim66 <salimhasanriad8@gmail.com>
2022-02-06 20:54:12 +0600

    Read Orders By Status

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -10,10 +10,10 @@ use App\Http\Controllers\Controller;
 class OrderController extends Controller
 {
     /**
-     * Peding Orders List
+     * Pending Orders List
      */
     public function pendingOrders(){
-        $orders = Order::where('status', 'Pending')->orderBy('id', 'DESC')->get();
+        $orders = Order::where('status', 'pending')->orderBy('id', 'DESC')->get();
         return view('backend.orders.pending_orders', compact('orders'));
     }
 
@@ -25,5 +25,54 @@ class OrderController extends Controller
     	$orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
         // dd($orderItem);
         return view('backend.orders.pending_order_details', compact('order', 'orderItem'));
+    }
+
+    /**
+     * confirmed Orders List
+     */
+    public function confirmedOrders(){
+        $orders = Order::where('status', 'confirmed')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.confirmed_orders', compact('orders'));
+    }
+
+    /**
+     * processing Orders List
+     */
+    public function processingOrders(){
+        $orders = Order::where('status', 'processing')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.processing_orders', compact('orders'));
+    }
+
+    /**
+     * picked Orders List
+     */
+    public function pickedOrders(){
+        $orders = Order::where('status', 'picked')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.picked_orders', compact('orders'));
+    }
+
+
+    /**
+     * shipped Orders List
+     */
+    public function shippedOrders(){
+        $orders = Order::where('status', 'shipped')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.shipped_orders', compact('orders'));
+    }
+
+    /**
+     * delivered Orders List
+     */
+    public function deliveredOrders(){
+        $orders = Order::where('status', 'delivered')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.delivered_orders', compact('orders'));
+    }
+
+      /**
+     * cancel Orders List
+     */
+    public function cancelOrders(){
+        $orders = Order::where('status', 'cancel')->orderBy('id', 'DESC')->get();
+        return view('backend.orders.cancel_orders', compact('orders'));
     }
 }
---
+++ resources/views/backend/orders/shipped_orders.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.admin_master')
+
+@section('main-content')
+<div class="container-full">
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+
+
+        <div class="col-12">
+
+         <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Shipped Order List</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Invoice</th>
+                            <th>Amount</th>
+                            <th>Payment</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($orders as $data)
+                        <tr>
+                            <td>{{ $data->order_date }}</td>
+                            <td>{{ $data->invoice_no }}%</td>
+                            <td>{{ $data->amount }}</td>
+                            <td>{{ $data->payment_method }}</td>
+                            <td><span class="badge badge-pill badge-success">{{ $data->status }}</span></td>
+                            <td width="25%">
+                                <a title="Order Details" href="{{ route('pending.order.detials', $data->id) }}" class="btn btn-info"><i class="fa fa-eye"></i></a>
+
+                                <a title="Delete Data" href="{{ route('coupon.delete', $data->id) }}" id="delete" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+        </div>
+
+        <!-- /.col -->
+
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+</div>
+@endsection
